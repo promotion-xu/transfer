a822d2fac9ee85fc578cc3816398068887f475e0
xuzhen <Black@appledeMacBook-Pro.local>
2019-01-06 15:04:06 +0800

    init project

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,13 @@
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue';
+export default class App extends Vue {
+
+}
+</script>
+
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
---
+++ src/views/Login/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    This is a login page
+  </div>
+</template>
+
+
+<script>
+import Vue from 'vue';
+export default class Login extends Vue{
+  
+}
+</script>
